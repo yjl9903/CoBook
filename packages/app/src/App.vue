5064c07b36a72038ac999c889f6b8e99ebaa5f3c
XLor <yjl9903@vip.qq.com>
2022-02-19 20:30:45 +0800

    chore: init app

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { NavBar, Icon } from 'vant';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<nav-bar fixed >
+  <template #left>
+    <Icon name="user-o" color="black" />
+  </template>
+  <template #title>
+    <span class="font-bold">Coin</span>
+  </template>
+  <template #right>
+    <Icon name="setting-o" color="black" />
+  </template>
+</nav-bar>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
