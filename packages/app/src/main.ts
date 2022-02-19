b9d546eb5d306202d4d5ef79c6bc223068b4afe2
XLor <yjl9903@vip.qq.com>
2022-02-20 00:33:43 +0800

    chore: format

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App.vue';
-import { router } from './router'
+import { router } from './router';
 
 registerSW({});
 
--- packages/app/src/pages/Home.vue
+++ packages/app/src/pages/Home.vue
@@ -7,11 +7,6 @@ const value = ref('');
 </script>
 
 <template>
-<field v-model="value" label="金额" readonly clickable @touchstart.stop="show = true" />
-<number-keyboard
-  v-model="value"
-  :show="show"
-  :maxlength="6"
-  @blur="show = false"
-/>
+  <field v-model="value" label="金额" readonly clickable @touchstart.stop="show = true" />
+  <number-keyboard v-model="value" :show="show" :maxlength="6" @blur="show = false" />
 </template>
