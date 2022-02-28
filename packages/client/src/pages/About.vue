226e824f162c25171abdea9d52affc72bd9940d4
XLor <yjl9903@vip.qq.com>
2022-02-28 13:51:28 +0800

    feat: show fingerprint id

--- packages/client/src/pages/About.vue
+++ packages/client/src/pages/About.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import IconGithub from '~icons/mdi/github';
+import { fingerprint } from '@/logic/fingerprint';
 
 const version = __VERSION__;
 const commit = __GITHUB_SHA__;
+const fg = ref('');
+
+fingerprint().then(({ visitorId }) => (fg.value = visitorId));
 </script>
 
 <template>
   <div class="text-center text-gray-400 pt-4">
+    <div class="mb-4" font="mono">FingerPrint: {{ fg }}</div>
     <div class="flex justify-center font-mono h-4">
       <a
         class="text-$text-light-1 inline-flex items-center mr-2"
