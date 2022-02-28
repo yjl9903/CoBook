b54516d7d527d978a9708bc89222de899baf15d8
XLor <yjl9903@vip.qq.com>
2022-02-28 21:35:53 +0800

    fix: include style in node_modules

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -35,7 +35,8 @@ export default defineConfig({
   plugins: [
     vue(),
     createStyleImportPlugin({
-      resolves: [VantResolve()]
+      resolves: [VantResolve()],
+      exclude: []
     }),
     VitePWA({
       registerType: 'autoUpdate',
