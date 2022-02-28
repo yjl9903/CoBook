a843aea2859d15466c7029911ee5132cbe4ae550
yjl9903 <yjl9903@users.noreply.github.com>
2022-02-28 03:26:02 +0000

    deploy: 980819cd2d10455e645527e239af126c7adfe3d9

---
+++ assets/index.d5f0c96e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.b36902aa.js";import{b as o,o as t,g as s,k as a}from"./vendor.ecf1a60c.js";const r={pt:"4"},i=o({setup(c){return(n,_)=>(t(),s("div",r,[a(e)]))}});export{i as default};
